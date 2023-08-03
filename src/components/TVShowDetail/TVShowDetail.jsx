c5a7c661a141be407da1b1912b9eebdb811526c7
ctomrp <cristian.rico.palma@gmail.com>
2023-08-02 23:05:48 -0400

    rating

--- src/components/TVShowDetail/TVShowDetail.jsx
+++ src/components/TVShowDetail/TVShowDetail.jsx
@@ -1,9 +1,16 @@
+import { FiveStarRating } from "../FiveStarRating/FiveStarRating";
 import s from "./style.module.css";
 
-export function TVShowDetail({ tvShow }){
-    return <div>
-        <div className={s.title}>{tvShow.name}</div>
-        <div className={s.rating}>{tvShow.vote_average/2}/5</div>
-        <div className={s.overview}>{tvShow.overview}</div>
-    </div>;
+export function TVShowDetail({ tvShow }) {
+    const rating = (tvShow.vote_average / 2).toFixed(1);
+  return (
+    <div>
+      <div className={s.title}>{tvShow.name}</div>
+      <div className={s.rating_container}>
+        <FiveStarRating rating={rating} />
+        <span className={s.rating}>{rating}/5</span>
+      </div>
+      <div className={s.overview}>{tvShow.overview}</div>
+    </div>
+  );
 }
