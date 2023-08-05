64620a2552ae636d8faafafbb2dbfadeec2687b7
ctomrp <cristian.rico.palma@gmail.com>
2023-08-04 21:05:00 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,62 @@ import s from './style.module.css';
 import { useState } from 'react';
 import { Logo } from './components/Logo/Logo';
 import logoImg from "./assets/images/logo.png"
+import { TVShowList } from './components/TVShowList/TVShowList';
+import { SearchBar } from './components/SearchBar/SearchBar';
 
 export function App(){
 
     const [currentTVShow, setCurrentTVShow] = useState();
+    const [recommendationList, setRecommendationList] = useState([]);
+
 
     async function fetchPopulars(){
-        const popularTVShowList = await TVShowAPI.fetchPopulars();
-        if(popularTVShowList.length > 0){
-            setCurrentTVShow(popularTVShowList[0]);
+        try{
+            const popularTVShowList = await TVShowAPI.fetchPopulars();
+            if(popularTVShowList.length > 0){
+                setCurrentTVShow(popularTVShowList[0]);
+            }
+        }catch(error){
+            alert("Something went wrong");
+        }
+    }
+    
+    async function fetchRecommendations(tvShowId){
+        try{
+            const recommendationsListResp = await TVShowAPI.fetchRecommendations(tvShowId);
+            if(recommendationsListResp.length > 0){
+                setRecommendationList(recommendationsListResp.slice(0,10));
+            }
+        }catch(error){
+            alert("Something went wrong");
+        }
+    }
+    
+    async function searchTVShow(tvShowName){
+        try {
+            const searchResponse = await TVShowAPI.fetchByTitle(tvShowName);
+            if(searchResponse.length > 0){
+                setCurrentTVShow(searchResponse[0]);
+            }
+        } catch (error) {
+            alert("Something went wrong");
         }
     }
 
     useEffect(()=>{
         fetchPopulars();
     },[]);
+    
+    useEffect(()=>{
+        if(currentTVShow){
+            fetchRecommendations(currentTVShow.id);
+        }
+    },[currentTVShow]);
 
-    console.log(currentTVShow);
-
+    function updateCurrentTVShow(tvShow){
+        setCurrentTVShow(tvShow);
+    }
+    
     return (
         <div className={s.main_container}
         style={{
@@ -39,14 +77,16 @@ export function App(){
                         <Logo image={logoImg} title={"Watowatch"} subtitle={"Find a show you may like"} />
                     </div>
                     <div className="col-md-12 col-lg-4">
-                        <input type="text" style={{width:"100%"}}/>
+                        <SearchBar onSubmit={searchTVShow} />
                     </div>
                 </div>
             </div>
             <div className={s.tv_show_detail}>
                 {currentTVShow && <TVShowDetail tvShow={currentTVShow} />}
             </div>
-            <div className={s.recommended_tv_shows}>Recommended tv show</div>
+            <div className={s.recommended_tv_shows}>
+                {currentTVShow && <TVShowList onClickItem={updateCurrentTVShow} tvShowList={recommendationList} />}
+            </div>
         </div>
     );
 }
